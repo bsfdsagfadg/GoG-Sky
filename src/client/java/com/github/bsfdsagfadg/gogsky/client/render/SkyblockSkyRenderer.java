f3dcc54a6426306da6032d5ff769a7c120c45a6a
bsfdsagfadg <918272281@qq.com>
2026-07-26 07:49:51 +0800

    fix: add depth mask control

--- src/client/java/com/github/bsfdsagfadg/gogsky/client/render/SkyblockSkyRenderer.java
+++ src/client/java/com/github/bsfdsagfadg/gogsky/client/render/SkyblockSkyRenderer.java
@@ -58,6 +58,7 @@ public class SkyblockSkyRenderer {
 	 */
 	public static void renderExtra(PoseStack ms, MultiBufferSource bufferSource, ClientLevel world, float partialTicks, float insideVoid) {
 		com.mojang.blaze3d.opengl.GlStateManager._disableDepthTest();
+		com.mojang.blaze3d.opengl.GlStateManager._depthMask(false);
 		float rain = 1.0F - world.getRainLevel(partialTicks);
 		float celAng = world.getTimeOfDay(partialTicks);
 		float effCelAng = celAng;
@@ -205,6 +206,7 @@ public class SkyblockSkyRenderer {
 			}
 		}
 		ms.popPose();
+		com.mojang.blaze3d.opengl.GlStateManager._depthMask(true);
 		com.mojang.blaze3d.opengl.GlStateManager._enableDepthTest();
 	}
 
